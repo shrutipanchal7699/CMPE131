411cffcd3618cd525269bd47a51421c75dbb1152
qnguyen3411 <qnguyen3411@gmail.com>
2019-10-03 12:03:22 -0700

    defined route for room list page

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,7 @@ def homePage():
 @app.route('/auth'):
 def authPage():
     return 'Login and Registration Page'
+
+@app.route('/rooms'):
+def roomListPage():
+    return 'page to filter and search for rooms'
