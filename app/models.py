bf1e01f00418dff10f64ee0668e01dfa7b3a4178
qnguyen3411 <qnguyen3411@gmail.com>
2019-10-17 22:17:54 -0700

    Login implemented with flask login

--- app/models.py
+++ app/models.py
@@ -1,17 +1,39 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.email)
 
     @classmethod
-    def create(cls, **kwargs):
-        new_user = cls(**kwargs)
+    def create(cls, email, password):
+        pw_hash = generate_password_hash(password, method='sha256')
+        new_user = cls(email=email, password_hash=pw_hash)
         db.session.add(new_user)
         db.session.commit()
         return new_user
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get(int(id))
+
+    #Accepts username and password, return the user if the login information is good
+    @classmethod
+    def check_login(cls, email, password):
+        user = cls.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password_hash, password):
+                return user
+
+        return None
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,31 @@
 from flask import render_template, redirect, url_for
-from werkzeug import generate_password_hash
 
-from app import app, db
+from app import app, db, login_manager
 from app.forms import LoginForm, RegisterForm
 from app.models import User
-
+from flask_login import login_user, current_user
 
 @app.route('/')
 def homePage():
     return render_template('home.html')
+
+#Set authPage as the default login view for FlaskLogin
+login_manager.login_view = 'authPage'
 
 @app.route('/auth', methods=['GET', 'POST'])
 def authPage():
     login_form = LoginForm()
     register_form = RegisterForm()
     if login_form.login_submit.data and login_form.validate():
-        print('POSTED LOGIN FORM')
-        return login_form.email.data + ' ' + login_form.password.data
+        user = User.check_login(email=login_form.email.data, password=login_form.password.data)
+        if user:
+            login_user(user, remember=login_form.remember.data)
+            return redirect(url_for('roomListPage'))
 
     if register_form.register_submit.data and register_form.validate():
-        pw_hash = generate_password_hash(register_form.password.data)
-        new_user = User.create(email=register_form.email.data, password_hash=pw_hash)
-        return 'New User created!' + new_user.email
+        new_user = User.create(email=register_form.email.data, password=register_form.password.data)
+        login_user(new_user)
+        return redirect(url_for('roomListPage'))
 
     data = {
         'login_form': login_form,
