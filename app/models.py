bb2367942d2b2094c3e5660b8d8df24835894c8f
qnguyen3411 <qnguyen3411@gmail.com>
2019-10-21 18:46:30 -0700

    removed username collumn

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ from app import db, login_manager
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
