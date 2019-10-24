b51d432789e36b1ac5841fc3983f139a4837be89
qnguyen3411 <qnguyen3411@gmail.com>
2019-10-24 16:33:58 -0700

    register admin page for room

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 #Hotel Callisto
+
+To update db:
+
+ - delete app.db if its eists
+ - in terminal: $flask shell
+              >>>from app import db
+              >>>db.create_all()
+              >>>exit()
--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,8 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 from app import app, db
-from app.models import User
+from app.models import User, Room
 
 admin = Admin(app, name='hotelApp')
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Room, db.session))
