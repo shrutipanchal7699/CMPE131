adb41b04de9d8a1661f87437f796702cdbb8cdac
shrutipanchal7699 <54422924+shrutipanchal7699@users.noreply.github.com>
2019-10-24 17:02:06 -0700

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,9 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 from app import app, db
-from app.models import User, Room
+from app.models import User, Room, Reservation
 
 admin = Admin(app, name='hotelApp')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Room, db.session))
+admin.add_view(ModelView(Reservation, db.session))
