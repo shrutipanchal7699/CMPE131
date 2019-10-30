47fd74a6d7721c5dd0790ffacbb5b0c32da91f91
qnguyen3411 <qnguyen3411@gmail.com>
2019-10-30 10:16:22 -0700

    moved route import up, removed migrate for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,21 @@
 from flask import Flask
 
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
 
 from app import config
 from app.models import db
 from app.login_manager import login_manager
 from app.admin import admin
+from app.routes import configure_routes
 
 def create_app(conf=config.Config):
     app = Flask(__name__)
     app.config.from_object(conf)
 
     db.init_app(app)
-    migrate = Migrate(app, db)
     login_manager.init_app(app)
     admin.init_app(app)
-
-    from app.routes import configure_routes
     configure_routes(app)
 
     return app
