e48ab9e5f183958198eb31bc00b9d498552017de
qnguyen3411 <qnguyen3411@gmail.com>
2019-11-06 13:03:20 -0800

    initialize db to route test

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,11 +1,21 @@
 from flask_testing import TestCase
+
 from app import create_app
 from app.config import TestConfig
+from app.models import db
+
 
 class RouteTest(TestCase):
 
     def create_app(self):
         return create_app(TestConfig)
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_home_page(self):
         response = self.client.get('/')
