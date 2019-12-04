395870352f2832a252f3c780134d874dd8a631b8
qnguyen3411 <qnguyen3411@gmail.com>
2019-12-04 11:59:39 -0800

    put flask-migrate back

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-
+from flask_migrate import Migrate
 
 from app import config
 from app.models import db
@@ -12,12 +12,14 @@ from app.admin import admin
 from app.routes import configure_routes
 
 csrf = CSRFProtect()
+migrate = Migrate()
  
 def create_app(conf=config.Config):
     app = Flask(__name__)
     app.config.from_object(conf)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     admin.init_app(app)
     csrf.init_app(app)
